ca143529b5bb4505ce29873fc380a1d95c2881b6
yozhgoor <yozhgoor@outlook.com>
2020-12-14 19:26:30 +0100

    Add static folder to the watch

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use anyhow::Result;
-use std::process::Command;
+use notify::{RecursiveMode, Watcher};
+use std::{collections::HashSet, iter::FromIterator, process::Command};
 use structopt::StructOpt;
 use wasm_run::prelude::*;
 
-#[wasm_run::main(pre_build = pre_build)]
+#[wasm_run::main(
+    pre_build = pre_build,
+    watch = watch,
+)]
 #[derive(StructOpt, Debug)]
 enum Cli {}
 
@@ -15,6 +19,24 @@ fn pre_build(_args: &DefaultBuildArgs, profile: BuildProfile, command: &mut Comm
         BuildProfile::Dev => {
             command.args(&["--features", "console_error_panic_hook"]);
         }
+    }
+
+    Ok(())
+}
+
+fn watch(args: &DefaultServeArgs, watcher: &mut RecommendedWatcher) -> Result<(), anyhow::Error> {
+    let metadata = args.build_args().metadata();
+
+    let _ = watcher.watch("static", RecursiveMode::Recursive);
+
+    let members: HashSet<_> = HashSet::from_iter(&metadata.workspace_members);
+
+    for package in metadata.packages.iter().filter(|x| members.contains(&x.id)) {
+        let _ = watcher.watch(&package.manifest_path, RecursiveMode::Recursive);
+        let _ = watcher.watch(
+            package.manifest_path.parent().unwrap().join("src"),
+            RecursiveMode::Recursive,
+        );
     }
 
     Ok(())
